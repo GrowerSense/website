7afe511840ec0e5878ec810ff3e7e81c5635c694
Craig Maas <camaas@gmail.com>
2014-05-17 14:50:25 -0700

    Initial commit of base node/express web framework

---
+++ app.js
@@ -0,0 +1,83 @@
+console.log("reached app.js file");
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var MONGOLAB_URI = 'mongodb://heroku_app24603319:8phs1m8e1ii703mrf7cjpbe5j@ds031777.mongolab.com:31777/heroku_app24603319';
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk(MONGOLAB_URI);
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log("static setup complete");
+
+// TODO: what does this do?
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+console.log("route setup complete");
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+console.log("error handling setup complete");
+
+module.exports = app;
+
+// heroku port is 5000
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
